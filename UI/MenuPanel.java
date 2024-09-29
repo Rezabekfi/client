813e3ee678b9ff60456a754a655757de229fbca7
Rezabekfi <filip.rezabek@gmail.com>
2024-09-29 19:42:47 +0200

    fghg

--- UI/MenuPanel.java
+++ UI/MenuPanel.java
@@ -1,11 +1,16 @@
 package UI;
-import java.awt.CardLayout;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import Settings.Constants;
 
 public class MenuPanel extends JPanel {
 
@@ -13,32 +18,46 @@ public class MenuPanel extends JPanel {
 
     public MenuPanel(WindowHandlerer mainFrame) {
         this.mainFrame = mainFrame;
-        JButton playButton = new JButton("Play Game");
-        playButton.addActionListener(new ActionListener() {
+        this.setLayout(new BorderLayout());
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
-                mainFrame.showCard("Game");
-            }
-            
-        });
-        this.add(playButton);
+        setUpMenuTitle();        
+        setUpMenuButtonPanel();
     }
-    /* 
-    public MenuPanel(JPanel mainPanel) {
-        this.mainPanel = mainPanel;
-        JButton playButton = new JButton("Play Game");
-        playButton.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                CardLayout cl = (CardLayout) mainPanel.getLayout();
-                cl.show(mainPanel, "Game");  // Switch to Panel 1
-            }
-            
-        });
-        this.add(playButton);
+    public void setUpMenuTitle(){
+        JPanel titlePanel = new JPanel();
+        titlePanel.setLayout(new BorderLayout());
+        JLabel title = new JLabel(Constants.GAME_NAME, SwingConstants.CENTER);
+        title.setFont(new Font("SansSerif", Font.BOLD, mainFrame.getHeight()/4));
+        titlePanel.add(title);
+        this.add(titlePanel, BorderLayout.CENTER);
     }
-    */
+
+    public void setUpMenuButtonPanel() {
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 1));
+        
+        JPanel playSoloPanel = new JPanel();
+        JButton playSolButton = new JButton("Play Solo");
+        playSoloPanel.add(playSolButton);
+
+        JPanel playMultiplayerPanel = new JPanel();
+        JButton playMultiplayerButton = new JButton("Play Online");
+        playMultiplayerPanel.add(playMultiplayerButton);
+
+        JPanel settingsPanel = new JPanel();
+        JButton settingsButton = new JButton("Settings");
+        settingsPanel.add(settingsButton);
+
+        JPanel exitPanel = new JPanel();
+        JButton exitButton = new JButton("Exit");
+        exitPanel.add(exitButton);
+
+        buttonPanel.add(playSoloPanel);
+        buttonPanel.add(playMultiplayerPanel);
+        buttonPanel.add(settingsPanel);
+        buttonPanel.add(exitPanel);
+
+        this.add(buttonPanel, BorderLayout.SOUTH);
+    }
 }
