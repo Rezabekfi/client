45e2f05acb735be0c35abb637a9e51597ffe9ff8
Rezabekfi <filip.rezabek@gmail.com>
2025-01-12 21:46:15 +0100

    ..

--- src/main/java/com/quoridor/GameLogic/MultiplayerGameManager.java
+++ src/main/java/com/quoridor/GameLogic/MultiplayerGameManager.java
@@ -339,13 +339,8 @@ public class MultiplayerGameManager extends GameManager {
     }
 
     private void handleError(GameMessage message) {
-        if (message.getMessage().equals("Invalid move")) {
-            // NOTE THIS IS A SPECIAL RULE NOT KNOWN BY A LOT OF PLAYERS, SO WE WONT KICK RIGHT AWAY
-            PopupWindow.showMessage("Invalid move. Please try again. This rule isn't well known. You CANNOT block opponent comepletely.");
-        } else {
-            message.setMessage("Server side error: " + message.getMessage());
-            handleWrongMessage(message);
-        }
+        message.setMessage("Server send error: " + message.getMessage());
+        handleWrongMessage(message);
     }
 
     synchronized private void handleWrongMessage(GameMessage message) {
